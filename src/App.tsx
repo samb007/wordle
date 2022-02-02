2fcc194fd5abfe98de84c1960b0e37e328654c21
Sam Burke <samuelburke404@gmail.com>
2022-02-02 20:52:28 +0000

    feat: submit new word attempts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import './App.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Input = {
+	attempt: string;
+};
 
 function App() {
-	return <div className="App">Hello world(le)</div>;
+	const { register, handleSubmit } = useForm<Input>();
+	const [solutionAttempts, setSolutionAttempts] = React.useState<string[]>([
+		'raise',
+		'waste',
+	]);
+
+	const onSubmit: SubmitHandler<Input> = ({ attempt }) => {
+		setSolutionAttempts([...solutionAttempts, attempt]);
+		console.log(attempt);
+	};
+
+	return (
+		<div>
+			<div className="flex justify-center flex-col items-center">
+				{solutionAttempts.map((solutionAttempt) => {
+					return (
+						<div className="m-4 flex flex-row">
+							{solutionAttempt.split('').map((letter) => {
+								return <div className="p-4 m-1 border">{letter}</div>;
+							})}
+						</div>
+					);
+				})}
+			</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input
+					type="text"
+					{...register('attempt', { required: true })}
+					pattern="[A-Za-z]{5}"
+				/>
+				<input type="submit" />
+			</form>
+		</div>
+	);
 }
 
 export default App;
